0ea0989e0091bdb5d825774a1b2d68aee82fdbb7
unknown <yefersson.zuniga.tecsup.edu.pe>
2023-10-14 20:58:33 -0500

    AMM Laboratorio 07 completo

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Linking } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const GITHUB_API_URL = 'https://api.github.com';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f0f0f0',
+    padding: 20,
+  },
+  header: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  userContainer: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
+    marginBottom: 15,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  userName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#333',
+  },
+  userUrl: {
+    fontSize: 16,
+    color: 'blue',
   },
 });
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(`${GITHUB_API_URL}/search/users?q=location:"Arequipa"`);
+        setUsers(response.data.items);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleUserClick = (url) => {
+    Linking.openURL(url);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Usuarios de GitHub en Arequipa</Text>
+      <FlatList
+        data={users}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handleUserClick(item.html_url)}>
+            <View style={styles.userContainer}>
+              <Text style={styles.userName}>{item.login}</Text>
+              <Text style={styles.userUrl}>{item.html_url}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
